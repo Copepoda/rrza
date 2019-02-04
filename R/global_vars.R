4420ccef0e418829739a3fc0843db64dc6ee6294
Nissa Ferm <ncf@copepod.local>
2019-02-04 15:18:21 -0800

    add global vars from fastrcat

--- R/global_vars.R
+++ R/global_vars.R
@@ -5,6 +5,7 @@
 #' @importFrom grDevices dev.off
 #' @importFrom grDevices png
 #' @importFrom stats filter
+#' @importFrom stats na.omit
 NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
@@ -12,4 +13,22 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c(".","BEAKER_VOLUME",
                                                         "EST_NUM_PERM3",
                                                         "GEAR_NAME",
                                                         "LAT", "LON",
-                                                        "RZA_TAXA"))
+                                                        "RZA_TAXA",
+                                                        "CRUISE", "DATE",
+                                                        "DEPTH",
+                                                        "DEPTH_BOTTOM",
+                                                        "FOCI_GRID", "HAUL_ID",
+                                                        "HAUL_NAME",
+                                                        "JELLY_FOULING",
+                                                        "NET",
+                                                        "PHYTOPLANKTON_FOULING",
+                                                        "SALINITY1",
+                                                        "SAMPLE_COLOR",
+                                                        "SORTER","STATION_HAUL",
+                                                        "STATION_NAME",
+                                                        "SUBSAMPLE_COUNT",
+                                                        "SUBSAMPLE_VOLUME",
+                                                        "TEMPERATURE1",
+                                                        "TIME",
+                                                        "TOTAL_COUNT",
+                                                        "VOLUME_FILTERED"))
